829721bfa3e28d1c21cb2948f779c96a147564b2
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-12-20 01:11:14 +0100

    add http example
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

---
+++ examples/http.rs
@@ -0,0 +1,33 @@
+#![feature(async_await, await_macro, futures_api)]
+#![feature(dbg_macro)]
+
+use std::io;
+
+use futures::StreamExt;
+use futures::executor;
+use futures::io::AsyncReadExt;
+use futures::io::AsyncWriteExt;
+
+use romio::{TcpListener};
+
+fn main() -> io::Result<()> {
+    executor::block_on(async {
+        let listener = TcpListener::bind(&"127.0.0.1:7878".parse().unwrap())?;
+        let mut incoming = listener.incoming();
+
+        println!("Listening on 127.0.0.1:7878");
+
+        while let Some(stream) = await!(incoming.next()) {
+            let stream = stream?;
+
+            juliex::spawn(async move {
+              let (mut reader, mut writer) = stream.split();
+              await!(writer.write_all(b"HTTP/1.1 200 OK\r\nContent-Length:0\r\n\r\n")).unwrap();
+              let mut buf = vec![];
+              await!(reader.read_to_end(&mut buf)).unwrap();
+            });
+        }
+
+        Ok(())
+    })
+}
